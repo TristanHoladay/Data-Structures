a1184bf73c3c31dc1907707ce263d645493aa3fa
TristanHoladay <tholaday777@gmail.com>
2020-01-13 19:00:20 -0600

    union find/disjoint set

---
+++ com/williamfiset/datastructures/unionfind/UnionFind.java
@@ -0,0 +1,110 @@
+package com.williamfiset.datastructures.unionfind;
+
+public class UnionFind {
+
+    //The number of elements in this union find
+    private int size;
+
+    //Used to tracked the size of each of the components
+    private int[] sz;
+
+    //id[i] points to the parent of i, if id[i] = i then i is a root node
+    private int[] id;
+
+    //Tracks the number of components in the union find
+    private int numComponents;
+
+    public UnionFind(int size) {
+        if (size <= 0 ) throw new IllegalArgumentException("Size <= 0 is not allowed");
+        
+        //*my notes* # of elements and # of components is equal until unions occur
+        this.size = numComponents = size; 
+        sz = new int[size];
+        id = new int[size];
+
+        for(int i = 0; i < size; i++) {
+            id[i] = i; //Link to itself (self root)
+            sz[i] = 1; //Each component is originally of size one
+        }
+    }
+
+    public int find(int p) {
+
+        //Find the roote of the component/set
+        //*my notes* it's checking if the index and the value at said index are still the same
+        //if they are then that index is the root 
+        //if not then the checked value becomes the index and the check repeats
+        //this works because, in unifying, an index's value is changed to the index of it's root node
+        int root = p;
+        while (root != id[root]) {
+            root = id[root];
+        }
+
+        //Compress the path leading back to the root.
+        //Doing this operation is called "path compression"
+        //and is what gives us amortized time complexity.
+        //*my notes* capture the current value at id[p]
+        //set value of id[p] to root
+        //set p to the previous value of id[p], which was captured in the "next" variable
+        //once p = the root no need to continue checking because the value of id[p] is already the root index
+        while (p != root) {
+            int next = id[p];
+            id[p] = root;
+            p = next;
+        }
+
+        return root;
+    }
+
+    //This is an alternative recursive formulation for the find method
+    // public int find(int p) {
+    // if (p == id[p]) return p;
+    // return id[p] = find(id[p])
+    // }
+
+    //Return whether or not the elements 'p' and
+    //'q' are in the same components/set.
+    public boolean connected(int p, int q) {
+    return find(p) == find(q);
+    }
+
+    //Return the size of the components/set 'p' belongs to
+    public int componentSize(int p) {
+        return sz[find(p)];
+    }
+
+    //Return the number of elements in this UnionFind/Disjoint set
+    public int size() {
+        return size;
+    }
+
+    //Returns the number of remaining components/sets
+    public int components() {
+        return numComponents;
+    }
+
+    //Unify the components/sets containing elements 'p' and 'q'
+    public void unify(int p, int q) {
+        int root1 = find(p);
+        int root2 = find(q);
+
+        //These elements are already in the same group
+        if(root1 == root2) return;
+
+        //Merge smaller component/set into the larger one
+        if(sz[root1] < sz[root2]) {
+            sz[root2] += sz[root1];
+            id[root1] = root2;
+        } else {
+            sz[root1] += sz[root2];
+            id[root2] = root1;
+        }
+
+        //Since the roots found are different we know that the
+        //number of components/sets has decreased by one
+        numComponents--;
+    }
+}
+
+
+
